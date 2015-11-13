b245667500fde3f222cbd1f1857459a0f8cba3ee
mikessh <mikhail.shugay@gmail.com>
2015-11-13 07:00:05 +0300

    Level filter (confidence level support)

---
+++ src/main/groovy/com/antigenomics/vdjdb/text/LevelFilter.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2013-{year} Mikhail Shugay (mikhail.shugay@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.antigenomics.vdjdb.text
+
+import com.antigenomics.vdjdb.db.Entry
+
+class LevelFilter extends TextFilter {
+    private double value
+
+    LevelFilter(String columnId, String value, boolean negative) {
+        super(columnId, value, negative)
+        if (!value.isDouble()) {
+            throw new RuntimeException("Numeric value should be provided to level filter ($value)")
+        }
+        this.value = value.toDouble()
+    }
+
+    @Override
+    protected boolean passInner(Entry entry) {
+        entry.value.isDouble() ? (negative ^ (value >= entry.value.toDouble())) : negative
+    }
+}
