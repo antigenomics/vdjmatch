56f5478f8a74d065522f1a5cda408ae3a9faf771
mikessh <mikhail.shugay@gmail.com>
2016-07-06 04:38:50 +0300

    Some cleanup

--- src/main/groovy/com/antigenomics/vdjdb/Util.groovy
+++ src/main/groovy/com/antigenomics/vdjdb/Util.groovy
@@ -59,15 +59,11 @@ class Util {
             return true
         }
 
-        return false
+        false
     }
 
     static InputStream resourceAsStream(String resourceName) {
         Util.class.classLoader.getResourceAsStream(resourceName)
-    }
-
-    static String replaceNonAa(String seq) {
-        seq.replaceAll(/[^FLSYCWPHQRIMTNKVADEG]/, "X")
     }
 
     static AminoAcidSequence convert(String aaSeq) {
@@ -76,97 +72,6 @@ class Util {
             return new AminoAcidSequence(aaSeq)
 
         System.err.println("Error converting '$aaSeq' to amino acid sequences, entry will be skipped from search")
-        return null
-    }
-
-    @CompileStatic
-    static String codon2aa(String codon) {
-        String codonUpper = codon.toUpperCase()
-        switch (codonUpper) {
-            case 'TTT': return 'F'
-            case 'TTC': return 'F'
-            case 'TTA': return 'L'
-            case 'TTG': return 'L'
-            case 'TCT': return 'S'
-            case 'TCC': return 'S'
-            case 'TCA': return 'S'
-            case 'TCG': return 'S'
-            case 'TAT': return 'Y'
-            case 'TAC': return 'Y'
-            case 'TAA': return '*'
-            case 'TAG': return '*'
-            case 'TGT': return 'C'
-            case 'TGC': return 'C'
-            case 'TGA': return '*'
-            case 'TGG': return 'W'
-            case 'CTT': return 'L'
-            case 'CTC': return 'L'
-            case 'CTA': return 'L'
-            case 'CTG': return 'L'
-            case 'CCT': return 'P'
-            case 'CCC': return 'P'
-            case 'CCA': return 'P'
-            case 'CCG': return 'P'
-            case 'CAT': return 'H'
-            case 'CAC': return 'H'
-            case 'CAA': return 'Q'
-            case 'CAG': return 'Q'
-            case 'CGT': return 'R'
-            case 'CGC': return 'R'
-            case 'CGA': return 'R'
-            case 'CGG': return 'R'
-            case 'ATT': return 'I'
-            case 'ATC': return 'I'
-            case 'ATA': return 'I'
-            case 'ATG': return 'M'
-            case 'ACT': return 'T'
-            case 'ACC': return 'T'
-            case 'ACA': return 'T'
-            case 'ACG': return 'T'
-            case 'AAT': return 'N'
-            case 'AAC': return 'N'
-            case 'AAA': return 'K'
-            case 'AAG': return 'K'
-            case 'AGT': return 'S'
-            case 'AGC': return 'S'
-            case 'AGA': return 'R'
-            case 'AGG': return 'R'
-            case 'GTT': return 'V'
-            case 'GTC': return 'V'
-            case 'GTA': return 'V'
-            case 'GTG': return 'V'
-            case 'GCT': return 'A'
-            case 'GCC': return 'A'
-            case 'GCA': return 'A'
-            case 'GCG': return 'A'
-            case 'GAT': return 'D'
-            case 'GAC': return 'D'
-            case 'GAA': return 'E'
-            case 'GAG': return 'E'
-            case 'GGT': return 'G'
-            case 'GGC': return 'G'
-            case 'GGA': return 'G'
-            case 'GGG': return 'G'
-            default:
-                if (codonUpper.contains("N") && codonUpper.length() == 3)
-                    return "X" // undefined
-                else
-                    return '?' // incomplete/missing
-        }
-    }
-
-    @CompileStatic
-    static String translateLinear(String seq, boolean reverse) {
-        def aaSeq = ""
-
-        if (reverse)
-            seq = seq.substring(seq.length() % 3)
-
-        for (int i = 0; i <= seq.size() - 3; i += 3) {
-            def codon = seq.substring(i, i + 3)
-            aaSeq += codon2aa(codon)
-        }
-
-        aaSeq
+        null
     }
 }
